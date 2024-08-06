aa02b520fbfa2006e00ce152097fd118180e7aeb
hrdevrrr <hacktest9633@gmail.com>
2024-08-07 01:48:26 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7340854055:AAG4jGFGY6W3M6JsKNf8VIXsYNvHydV99Wg")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("HRESPORTOWNER")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1381828828")
+
+
+
+
+
+
+
+  
